71c82505becd6aca6fb0850671f99e02be4d3e15
bahromiddin01 <bahromiddinabduqaxxorov1604@gmail.com>
2024-12-03 13:21:24 +0500

    initial commit

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -42,7 +42,7 @@ export default function LogInPage() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: window.location.origin
+                    redirectTo: 'https://interview-react-app-beta.vercel.app/'
                 }
             })
 
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -67,7 +67,7 @@ export default function SignUpPage() {
             const { error } = await supabase.auth.signInWithOAuth({
                 provider: 'google',
                 options: {
-                    redirectTo: window.location.origin
+                    redirectTo: 'https://interview-react-app-beta.vercel.app/'
                 },
             });
 
